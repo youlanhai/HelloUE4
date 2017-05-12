0f7ce530faafdfc93e26efddc1e1f9390c6c9535
youlanhai <you_lan_hai@foxmail.com>
2017-05-12 22:23:06 +0800

    初始化工程

---
+++ HelloUE4/Source/HelloUE4/HelloUE4.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HelloUE4.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HelloUE4, "HelloUE4" );
---
+++ HelloUE4/Source/HelloUE4/MyActor.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestUE4.h"
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+    FVector NewLocation = GetActorLocation();
+    float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+    NewLocation.Z += DeltaHeight * 20.0f;      //把高度以20的系数进行缩放
+    RunningTime += DeltaTime;
+    SetActorLocation(NewLocation);
+}
